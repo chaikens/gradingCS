853e1883d023127313d7a678de8c593c6028f26f
Seth Chaiken <sdc@cs.albany.edu>
2012-03-18 20:30:49 -0400

    Developing and testing test cases.

--- old/java/310grading/pr5/scripts/run.sh
+++ old/java/310grading/pr5/scripts/run.sh
@@ -7,6 +7,7 @@ export TANAME=chaikens
 PATH=$PATH:.  #Old fashioned setting.
 
 ./scriptIG.pl `pwd`/../submissions/student.2012-03-03-04-57-02
-./scriptIG.pl `pwd`/../submissions/multMain.2012-03-03-04-57-02
-./scriptIG.pl `pwd`/../submissions/nullCrash.2012-03-03-04-57-02
+#./scriptIG.pl `pwd`/../submissions/multMain.2012-03-03-04-57-02
+#./scriptIG.pl `pwd`/../submissions/nullCrash.2012-03-03-04-57-02
+
 
